965cf2bddae3af1150c473c8826db5d2bbc61871
Amanda Meneguini <admcmarshmallow@gmail.com>
2024-02-16 17:24:46 -0300

    responsive layout

--- src/js/acordeon.js
+++ src/js/acordeon.js
@@ -4,16 +4,22 @@ const itemAcordeon = document.querySelectorAll(".acordeon");
 
 itemAcordeon.forEach(itemAcordeon => {
     itemAcordeon.addEventListener('click', function(){
-        const acordeonEstaAberto = document.querySelector(".mostrar");
 
-        if(acordeonEstaAberto){
-            acordeonEstaAberto.classList.remove("mostrar");
-        }
+        
+        fecharOutrosAcordeons();
         
         itemAcordeon.classList.add("mostrar");
+        
 
     });
 });
 
 
 
+function fecharOutrosAcordeons(){
+    const algumAcordeonEstaAberto = document.querySelector(".mostrar");
+
+        if(algumAcordeonEstaAberto){
+            algumAcordeonEstaAberto.classList.remove("mostrar");
+        }
+}
